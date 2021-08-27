0753085c0b81e88bd2b8fc15857114c46b800b5d
Matheus Gaspar <msouzagaspar@gmail.com>
2021-08-27 15:58:48 +0200

    add image animation and shadow

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,12 +5,15 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-theme-ui',
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
           `quicksand\:400,500,700`,
-          `Signika:300`,
+          `Signika:300,400`,
           `Catamaran:wght@100;200;300;400`,
           `Nunito+Sans:wght@300;400`,
         ],
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,20 @@
 import { jsx } from 'theme-ui';
 import Header from '../components/Header';
 import { Link } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
+import { keyframes } from '@emotion/react';
+import img from '../images/profilePhoto.jpg';
+import { auto } from 'async';
+
+const morph = keyframes({
+  '0%': {
+    borderRadius: '60% 40% 30% 70% / 60% 30% 70% 40%',
+  },
+
+  '50%': {
+    borderRadius: '30% 60% 70% 40% / 50% 60% 30% 60%',
+  },
+});
 
 const IndexPage = () => {
   return (
@@ -53,8 +67,10 @@ const IndexPage = () => {
             flexGrow: 1,
             p: 2,
             pl: 4,
+            display: 'flex',
+            justifyContent: 'space-between',
 
-            '> p': {
+            p: {
               fontFamily: 'Signika',
               // fontFamily: 'Source+Sans+Pro',
               fontWeight: '300',
@@ -63,37 +79,75 @@ const IndexPage = () => {
             },
           }}
         >
-          <h1
+          <section>
+            <h1
+              sx={{
+                fontFamily: 'Nunito+Sans',
+                letterSpacing: -0.5,
+                color: t => t.colors.gray[9],
+                fontWeight: '500',
+                fontSize: t => t.fontSizes[3],
+                m: 0,
+              }}
+            >
+              Hi! My name is Matheus 👋
+            </h1>
+            <p>
+              I'm a 29-year-old engineer from Brazil, taking the first steps in
+              my career as a software developer. Currently, I'm studying front
+              and back-end web development at the awesome school Trybe, with
+              focus on JavaScript, React and Node.js.
+            </p>
+            <p>
+              I am fascinated about how computers can make our lives easier,
+              solve annoying problems and even help us build great habits. They
+              can also provide us beautiful and life-changing experiences! And
+              one of the things I enjoy the most is making these things happen.
+              When I'm not coding or studying, you'll probably find me cooking,
+              enjoying nature, traveling or spending some time with family and
+              friends. Always with a good cup a coffee.
+            </p>
+            <p>
+              It's been one year since I left Civil Engineering to dive deep
+              into the world of software, and it's just the beginning of an
+              amazing journey.
+            </p>
+          </section>
+          <div
             sx={{
-              fontFamily: 'Nunito+Sans',
-              letterSpacing: -0.5,
-              color: t => t.colors.gray[9],
-              fontWeight: '500',
-              fontSize: t => t.fontSizes[3],
-              m: 0,
+              position: 'relative',
             }}
           >
-            Hi! My name is Matheus 👋
-          </h1>
-          <p>
-            I'm a 29-year-old engineer from Brazil, taking the first steps in my
-            career as a software developer. Currently, I'm studying front and
-            back-end web development at the awesome school Trybe, with focus on
-            JavaScript, React and Node.js.
-          </p>
-          <p>
-            I am fascinated about how computers can make our lives easier, solve
-            annoying problems and even help us build great habits. And one of
-            the things I enjoy the most is making these things happen. When I'm
-            not coding or studying, you'll probably find me cooking, enjoying
-            nature, traveling or spending some time with family and friends.
-            Always with a good cup a coffee.
-          </p>
-          <p>
-            It's been one year since I left Civil Engineering to dive deep into
-            the world of software, and it's just the beginning of an amazing
-            journey.
-          </p>
+            <StaticImage
+              src="../images/profilePhoto.jpg"
+              layout="fixed"
+              alt="A photo of Matheus"
+              width={150}
+              placeholder="blurred"
+              sx={{
+                flexShrink: 0,
+                ml: 4,
+                mt: 3,
+                animation: `${morph} 16s ease-in-out infinite`,
+                borderRadius: '60% 40% 30% 70% / 60% 30% 70% 40%',
+                transition: 'all 1s ease-in-out',
+              }}
+            />
+            <div
+              sx={{
+                position: 'absolute',
+                top: '16px',
+                right: '-10px',
+                background: 'linear-gradient(45deg, #88D5BF 0%, #5D6BF8 100%)',
+                animation: `${morph} 16s ease-in-out infinite`,
+                borderRadius: '60% 40% 30% 70% / 60% 30% 70% 40%',
+                transition: 'all 1s ease-in-out',
+                zIndex: -1,
+                width: '160px',
+                height: '300px',
+              }}
+            ></div>
+          </div>
         </main>
       </div>
     </div>
