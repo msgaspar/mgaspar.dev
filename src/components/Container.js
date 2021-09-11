37cb92f376f5bf86f20a35c7a402dae78cdcb63e
Matheus Gaspar <msouzagaspar@gmail.com>
2021-09-11 12:48:39 +0200

    add background pattern

--- src/components/Container.js
+++ src/components/Container.js
@@ -6,6 +6,7 @@ import Navbar from './Navbar';
 function Container({ children }) {
   return (
     <ContainerLayout>
+      <BackgroundPattern />
       <Header />
       <BodyLayout>
         <Content>{children}</Content>
@@ -15,14 +16,31 @@ function Container({ children }) {
   );
 }
 
+const BackgroundPattern = () => (
+  <div
+    sx={{
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      zIndex: '-999',
+      backgroundImage: t =>
+        `radial-gradient(${t.colors.bgPattern1} 0.9px, transparent 0.9px), radial-gradient(${t.colors.bgPattern1} 0.9px, ${t.colors.bgPattern2} 0.9px)`,
+      backgroundSize: '36px 36px',
+      backgroundPosition: '0 0,18px 18px',
+    }}
+  />
+);
+
 const ContainerLayout = ({ children }) => (
   <div
     sx={{
       display: 'flex',
       flexDirection: 'column',
       alignItems: 'center',
-      width: '100%',
+      width: 'fit-content',
       minHeight: '100%',
+      backgroundColor: t => t.colors.background,
+      mx: 'auto',
     }}
   >
     {children}
@@ -35,8 +53,8 @@ const BodyLayout = ({ children }) => (
       flexGrow: 1,
       display: 'flex',
       justifyContent: 'center',
-      width: '100%',
-      maxWidth: t => t.breakpoints[1],
+      width: t => t.breakpoints[1],
+      mx: t => t.space[4],
     }}
   >
     {children}
