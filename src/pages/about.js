87e89411e8e4dea821c687502f9e373efb8ab3fc
Matheus Gaspar <msouzagaspar@gmail.com>
2021-09-15 00:00:51 +0200

    create about me page

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
+
+import Container from '../components/Container';
+
+function AboutPage() {
+  return (
+    <Container>
+      <div>about</div>
+    </Container>
+  );
+}
+
+export default AboutPage;
