ab9bf31f2e20a0a0d93e58f299bf83c812e21660
Matheus Gaspar <msouzagaspar@gmail.com>
2021-09-15 00:01:06 +0200

    create projects page

---
+++ src/pages/projects.js
@@ -0,0 +1,17 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
+
+import Container from '../components/Container';
+import PageTitle from '../components/PageTitle';
+
+function ProjectsPage() {
+  return (
+    <Container>
+      <div>
+        <PageTitle title="Projects" />
+      </div>
+    </Container>
+  );
+}
+
+export default ProjectsPage;
