35c497d47ca095c63024b51f2bf0e6ec9073e469
Matheus Gaspar <msouzagaspar@gmail.com>
2021-10-01 13:30:34 +0200

    adjustments to project thumbnail

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,14 +3,49 @@ import { jsx } from 'theme-ui';
 import { StaticImage } from 'gatsby-plugin-image';
 
 import Container from '../components/Container';
-import PageTitle from '../components/PageTitle';
 
 function ProjectsPage() {
   return (
     <Container>
-      <div>
-        <PageTitle title="Projects" />
-        <StaticImage src="../images/zefirula-thumb.png" />
+      <div
+        sx={{
+          position: 'relative',
+          mt: 5,
+          width: '100%',
+          height: '360px',
+        }}
+      >
+        <div
+          sx={{
+            position: 'absolute',
+            width: '95%',
+            height: '95%',
+            margin: 'auto',
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            overflow: 'hidden',
+          }}
+        >
+          <StaticImage src="../images/zefirula-thumb.png" />
+        </div>
+        <h2
+          sx={{
+            position: 'absolute',
+            fontFamily: 'heading',
+            t: 0,
+            l: 0,
+            backgroundColor: '#252525',
+            color: 'white',
+            px: 3,
+            py: 1,
+            fontSize: t => t.fontSizes[5],
+            mt: '-5px',
+          }}
+        >
+          Zé Firula
+        </h2>
       </div>
     </Container>
   );
