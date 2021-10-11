75869e1e3d6df5fcab06262dd5999285e979becb
Matheus Gaspar <msouzagaspar@gmail.com>
2021-10-11 11:29:47 +0200

    rename container component to layout

---
+++ src/components/Layout.js
@@ -0,0 +1,57 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
+import { Header } from './Header';
+
+function Layout({ children }) {
+  return (
+    <ContainerLayout>
+      <Header sx={{ marginY: 3 }} />
+      <BodyLayout>
+        <Content>{children}</Content>
+      </BodyLayout>
+    </ContainerLayout>
+  );
+}
+
+const ContainerLayout = ({ children }) => (
+  <div
+    sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      width: 'fit-content',
+      minHeight: '100%',
+      backgroundColor: t => t.colors.background,
+      mx: 'auto',
+      boxShadow: t => t.colors.bgShadow,
+    }}
+  >
+    {children}
+  </div>
+);
+
+const BodyLayout = ({ children }) => (
+  <div
+    sx={{
+      flexGrow: 1,
+      display: 'flex',
+      justifyContent: 'center',
+      width: t => t.breakpoints[1],
+      mx: t => t.space[6],
+    }}
+  >
+    {children}
+  </div>
+);
+
+const Content = ({ children }) => (
+  <main
+    sx={{
+      flex: 1,
+    }}
+  >
+    {children}
+  </main>
+);
+
+export default Layout;
