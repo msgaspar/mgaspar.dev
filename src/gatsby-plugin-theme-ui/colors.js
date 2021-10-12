390f91b5a4ae480018221a662ae2334bb6b78594
Matheus Gaspar <msouzagaspar@gmail.com>
2021-10-12 09:58:32 +0200

    add colors for the project type badges

--- src/gatsby-plugin-theme-ui/colors.js
+++ src/gatsby-plugin-theme-ui/colors.js
@@ -1,14 +1,32 @@
 module.exports = {
   colors: {
     background: '#FFF',
+    offset: '#FAFAFA',
+    lightGray: '#e4e4e7',
     text: '#000',
     heading: '#000',
     muted: '#8a91a0',
-    primary: '#609',
+    primary: '#553C9A',
+    primaryOffset: '#E9D8FD',
     secondary: '#306',
     link: '#f83735',
     hoverLink: '#DD6B20',
     trybe: '#018353',
+
+    badges: {
+      green: {
+        color: '#22543D',
+        bg: '#C6F6D5',
+      },
+      orange: {
+        color: '#B45309',
+        bg: '#FDE68A',
+      },
+      purple: {
+        color: '#44337A',
+        bg: '#E9D8FD',
+      },
+    },
   },
   darkModeColors: {
     text: '#f8f8f2',
