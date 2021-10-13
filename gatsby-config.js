aba4c95d602977fba8366785bae2bc98b00d5ed9
Matheus Gaspar <msouzagaspar@gmail.com>
2021-10-13 15:52:51 +0200

    replace backticks with single quotes

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,11 +5,12 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-theme-ui',
-    `gatsby-plugin-image`,
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-plugin-google-fonts`,
+      resolve: 'gatsby-plugin-google-fonts',
       options: {
         fonts: [`Signika:300,400,600`, `Nunito+Sans:400,600,700,800`],
         display: 'swap',
