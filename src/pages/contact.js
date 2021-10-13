6a784996317a8867bb627e7646adfe9acbeafcf0
Matheus Gaspar <msouzagaspar@gmail.com>
2021-10-13 15:06:39 +0200

    add contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,31 @@
+/**@jsx jsx */
+import { jsx } from 'theme-ui';
+
+import Layout from '../components/Layout';
+import { PageSection, SectionTitle, TextContent } from '../components/lib';
+import { socialLinks } from '../utils/socialLinks';
+
+function ContactPage() {
+  return (
+    <Layout>
+      <PageSection>
+        <SectionTitle>Get in touch</SectionTitle>
+        <TextContent sx={{ maxWidth: '500px' }}>
+          <p>
+            Send me an email if you're looking for a developer, if you have any
+            questions, or if you just want to say hi.
+          </p>
+          <p>
+            <a href="mailto:msgaspar@outlook.com">msgaspar@outlook.com</a>
+          </p>
+          <p>
+            You can also find me on <a href={socialLinks.github.link}>GitHub</a>{' '}
+            and <a href={socialLinks.linkedin.link}>LinkedIn</a>.
+          </p>
+        </TextContent>
+      </PageSection>
+    </Layout>
+  );
+}
+
+export default ContactPage;
