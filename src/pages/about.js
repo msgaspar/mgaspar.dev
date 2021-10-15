42875a43efaa685ed1f689aae973ea6d422176a5
Matheus Gaspar <msouzagaspar@gmail.com>
2021-10-15 13:09:42 +0200

    update about me text

--- src/pages/about.js
+++ src/pages/about.js
@@ -33,23 +33,24 @@ function AboutPage() {
               developer who's fascinated about how technology can make our lives
               easier, solve annoying problems and help making the world a better
               place. And one of the things I enjoy the most is being able to
-              make these things happen by building quality software. I also love
-              enjoying nature, traveling, cooking, reading a good book and
-              spending time with my family and friends.
+              make these things happen by writing code. I also love enjoying
+              nature, traveling, cooking, reading a good book and spending time
+              with my family and friends.
             </p>
             <p>
               I graduated in Civil Engineering in 2016 and earned a master's
               degree in Transportation Engineering in 2019, both at the
               University of São Paulo. During those years I dedicated myself to
               scientific research, and soon after that I decided on a career
-              change to work with something I really enjoyed - programming.
+              change to work with something I knew I really enjoyed -
+              programming.
             </p>
             <p>
-              I'm currently studying for a degree in System Analysis and
-              Development at the Senac University Center, and entering the final
-              module of the web development course at <TrybeLink />. I'm also
-              looking for my first developer job opportunity. It's just the
-              beginning of an amazing journey.
+              Currently, I'm studying for a degree in System Analysis and
+              Development at the Senac University Center, and I'm entering the
+              final module of the web development course at <TrybeLink />. I'm
+              also looking for my first developer job opportunity, and I feel
+              this is just the beginning of an amazing journey.
             </p>
           </TextContent>
           <StaticImage
